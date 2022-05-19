48df206b02789a4de2b34948ec730313a6f3d043
Hamza Khan <hamza.kh28@gmail.com>
2022-05-19 14:54:41 +0100

    about 80% complete

--- src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -4,11 +4,12 @@ package com.bnta.chocolate.controllers;
 import com.bnta.chocolate.models.Chocolate;
 import com.bnta.chocolate.repositories.ChocolateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("chocolates")
@@ -18,8 +19,20 @@ public class ChocolateController {
     ChocolateRepository chocolateRepository;
 
     @GetMapping
-    public List<Chocolate> getAllChocolates(){
-        return chocolateRepository.findAll();
+    public ResponseEntity<List<Chocolate>> getAllChocolates(){
+        return new ResponseEntity<>(chocolateRepository.findAll(), HttpStatus.OK);
+    }
+    // SHOW route
+
+    @GetMapping(value = "/{id}") //localhost:8080/chocolates/1
+    public ResponseEntity<Optional<Chocolate>> getChocolate(@PathVariable Long id) {
+        return new ResponseEntity<>(chocolateRepository.findById(id), HttpStatus.OK);
     }
 
+//    POST route
+@PostMapping
+    public ResponseEntity<Chocolate> createChocolate(@RequestBody Chocolate newChocolate) {
+        chocolateRepository.save(newChocolate);
+    return new ResponseEntity<>(newChocolate, HttpStatus.CREATED);
+    }
 }
